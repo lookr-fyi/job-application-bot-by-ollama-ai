6e4333aafeb125530b11a780a8b588acf2d1d2ac
yuqil-lookr-fyi <yuqil@lookr.fyi>
2025-03-10 16:57:51 -0400

    updated download link

--- Casks/jobhuntr.rb
+++ Casks/jobhuntr.rb
@@ -1,8 +1,8 @@
 cask "jobhuntr" do
   version "1.0.0"
-  sha256 "SHA256_HASH_OF_THE_ARCHIVE"
+  sha256 "5e0abcbbceaf6eed8fd1d8d79db30125a6189e599c8baa789910463c1d0de5a0"
 
-  url "https://github.com/lookr-fyi/jobhuntr-app/releases/download/v1.0.0/JobHuntr_1.0.0.dmg"
+  url "https://github.com/lookr-fyi/homebrew-jobhuntr/releases/download/v1.0.0/JobHuntr_1.0.0.dmg"
   name "JobHuntr"
   desc "JobHuntr is a private ai agent that helps you get job interview opportunities"
   homepage "https://lookrfyi.notion.site/jobhuntr"
